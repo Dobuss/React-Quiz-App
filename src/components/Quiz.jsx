fbc1d6c9c909429673b4a4f1ade5aa106858322f
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-08 15:19:46 +0200

    shuffle questions logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCOmpleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prev) => {
@@ -11,12 +13,24 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCOmpleteImg} alt="Quiz Completed" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5); //shuffle
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
