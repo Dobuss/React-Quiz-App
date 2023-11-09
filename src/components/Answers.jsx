1ed244811746711a2ff502d1078c46bb3e633391
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-09 14:43:23 +0200

    reworking Q&A logic

---
+++ src/components/Answers.jsx
@@ -0,0 +1,40 @@
+import { useRef } from "react";
+export default function Answers({
+  answers,
+  selectedAnswer,
+  answerState,
+  onSelect,
+}) {
+  const shuffledAnswers = useRef(); //can be used to manage values independently from component lifecycle
+
+  if (!shuffledAnswers.current) {
+    shuffledAnswers.current = [...answers];
+    shuffledAnswers.current.sort(() => Math.random() - 0.5); //shuffle
+  }
+
+  return (
+    <ul id="answers">
+      {shuffledAnswers.current.map((answer) => {
+        const isSelected = selectedAnswer === answer;
+        let cssClass = "";
+        if (answerState === "answered" && isSelected) {
+          cssClass = "selected";
+        }
+        if (
+          (answerState === "correct" || answerState === "wrong") &&
+          isSelected
+        ) {
+          cssClass = answerState;
+        }
+
+        return (
+          <li key={answer} className="answer">
+            <button className={cssClass} onClick={() => onSelect(answer)}>
+              {answer}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,36 @@
 import { useCallback, useState } from "react";
 import QUESTIONS from "../questions.js";
 import quizCOmpleteImg from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    selectedAnswer
-  ) {
-    setUserAnswers((prev) => {
-      return [...prev, selectedAnswer];
-    });
-  },
-  []);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setAnswerState("answered");
+      setUserAnswers((prev) => {
+        return [...prev, selectedAnswer];
+      });
+
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
 
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
@@ -30,29 +45,18 @@ export default function Quiz() {
       </div>
     );
   }
-
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5); //shuffle
-
+  
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-        key={activeQuestionIndex} 
-        timeout={10000} 
-        onTimeout={handleSkipAnswer} 
-        />
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Question
+      key={activeQuestionIndex}
+      questionText={QUESTIONS[activeQuestionIndex].text}
+      answers={QUESTIONS[activeQuestionIndex].answers}
+      answerState={answerState}
+      selectedAnswer={userAnswers[userAnswers.length - 1]}
+      onSelectAnswer={handleSelectAnswer}
+      onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 }
